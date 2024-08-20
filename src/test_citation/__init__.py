9462791dd7cb0647a6818272bdb5545a407dbec3
Faruk <fdiblen@jokerXPS15.localdomain>
2024-08-20 16:54:22 +0200

    first commit

---
+++ src/test_citation/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about test_citation."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Jane Smith"
+__email__ = "yourname@esciencecenter.nl"
+__version__ = "0.1.0"
